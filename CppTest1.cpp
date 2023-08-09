47463b7c5ab8897993a2b477156c016fc2be4de7
onatuzunyayla <79927273+onatuzunyayla@users.noreply.github.com>
2023-08-09 22:05:59 +0300

    Initial Commit

---
+++ CppTest1.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CppTest1
+{
+	TEST_CLASS(CppTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			system("cmd.exe");
+		}
+	};
+}
